8037887380b45abe1e5d536d38e338109611e03b
hajime-goshima <goshima.hajime@gmail.com>
2019-04-07 23:17:17 +0900

    もっと見るをボタン実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,57 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
+    <div class="welcome">
       <h1 class="title">
-        pinterest-ui
+        Pinterest-UI
       </h1>
       <h2 class="subtitle">
-        My wondrous Nuxt.js project
+        Nuxt.jsでPisterestのようなページ遷移を実装する
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
+      <div class="arrow"></div>
+    </div>
+    <div class="item-list">
+      <div v-for="item in items" :key="item.id" class="item">
+        <h3>{{ item.title }}</h3>
+        <p>{{ item.body }}</p>
       </div>
+      <button class="readmore" @click="readMore">もっと読み込む</button>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      items: [
+        { id: 1, title: 'Title1', body: 'Body1' },
+        { id: 2, title: 'Title2', body: 'Body2' },
+        { id: 3, title: 'Title3', body: 'Body3' }
+      ]
+    }
+  },
+  methods: {
+    readMore() {
+      for (let i = 1; i <= 3; i++) {
+        const id = this.items.length + i
+        this.items.push({ id: id, title: `Title${id}`, body: `Body${id}` })
+      }
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .container {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.welcome {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
@@ -61,7 +75,32 @@ export default {
   padding-bottom: 15px;
 }
 
-.links {
+.arrow {
+  position: absolute;
+  bottom: 50px;
+  border-right: #7f828b 3px solid;
+  border-bottom: #7f828b 3px solid;
+  height: 50px;
+  width: 50px;
+  transform: rotate(45deg);
+}
+
+.item-list .item {
+  border: 1px solid #ddd;
+  padding: 50px;
+}
+.item-list .item h3 {
+  font-size: 30px;
+}
+.item-list .item p {
   padding-top: 15px;
+}
+
+.readmore {
+  display: block;
+  padding: 15px 50px;
+  font-size: 18px;
+  background: orange;
+  margin: 15px auto;
 }
 </style>
